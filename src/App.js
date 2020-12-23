bf9cb40b312060de046a974a7dc28a4225e8f19c
Marian Lyzhychka <52856608+mariankomen@users.noreply.github.com>
2020-12-23 11:38:28 +0200

    _

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App(props) {
                 <Route path='/music' render={ () => <Music/>}/>
                 <Route path='/news' render= { () => <News NewsItems={props.state.NewsComp.NewsItems}/>}/>
                 <Route path='/settings' component={Settings}/>
-                {/*asdfa*/}
+                {/*slozhno pizda*/}
             </div>
         </div>
         </BrowserRouter>
